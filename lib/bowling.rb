743f1e8dff06652796b9738dfe2b3e08f8a6cea0
Kevin Knowles <knowles.kevin@gmail.com>
2015-04-25 19:54:19 -0500

    Handles all ones

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,8 +1,13 @@
 class Bowling
   def initialize rolls
+    @rolls = rolls
   end
 
   def score
-    0
+    score = 0
+    @rolls.each_char do |roll|
+      score += roll.to_i
+    end
+    score 
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,4 +3,9 @@ describe Bowling, "#score" do
     game = Bowling.new '--------------------'
     expect(game.score).to eq(0)
   end
+
+  it 'returns 20 for all ones' do
+    game = Bowling.new '11111111111111111111'
+    expect(game.score).to eq(20)
+  end
 end
