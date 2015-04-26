a49dedc086b5b3e8d118a334e078839fab0ed35a
Kevin Knowles <knowles.kevin@gmail.com>
2015-04-25 20:01:11 -0500

    Refactor spare

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -7,13 +7,21 @@ class Bowling
     score = 0
     current_roll_index = 0
     1.upto(10) do |frame|
-      if @rolls[current_roll_index + 1] == '/'
-        score += 10 + @rolls[current_roll_index + 2].to_i
+      if is_spare current_roll_index
+        score += 10 + spare_bonus(current_roll_index)
       else  
         score += @rolls[current_roll_index].to_i + @rolls[current_roll_index+1].to_i
       end
       current_roll_index += 2
     end
     score 
+  end
+
+  def is_spare roll_index
+    @rolls[roll_index + 1] == '/'
+  end
+
+  def spare_bonus roll_index
+    @rolls[roll_index + 2].to_i
   end
 end
